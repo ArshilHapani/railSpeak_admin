3c68ef7958b5191afafab32650801c5dd8507105
ArshilHapani <arshilhapani998@gmail.com>
2024-04-04 03:58:36 +0530

    Minor refactoring

--- src/app/experiment/page.tsx
+++ src/app/experiment/page.tsx
@@ -8,28 +8,19 @@ const Testing = () => {
   const recognition = useRef<SpeechRecognition | null>(null);
   const textOutputRef = useRef<HTMLParagraphElement>(null);
 
-  // Function to handle speech recognition
   const startSpeechRecognition = () => {
-    // Check if speech recognition is available in the browser
     if ("SpeechRecognition" in window || "webkitSpeechRecognition" in window) {
       recognition.current = new (window.SpeechRecognition ||
         window.webkitSpeechRecognition)();
-
-      // Event listener for when speech is recognized
+      recognition.current.continue = true;
       recognition.current.onresult = (event: any) => {
-        // Get the recognized text from the event
         const transcript = event.results[0][0].transcript;
-
-        // Update the output element with the recognized text
         if (textOutputRef.current) {
           textOutputRef.current.textContent = transcript;
         }
       };
-
-      // Start speech recognition
       recognition.current.start();
     } else {
-      // Speech recognition not supported, handle accordingly
       console.error("Speech recognition not supported in this browser");
     }
   };
