3bd4145c41aa38a4c95a1fa9149921eb55a45b33
ArshilHapani <arshilhapani998@gmail.com>
2024-04-04 04:29:24 +0530

    change #1

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -31,7 +31,7 @@ const HomePage = async () => {
         </div>
       ) : (
         <>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5 mt-5">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5">
             {stations.map((station) => (
               <div
                 key={station.id}
